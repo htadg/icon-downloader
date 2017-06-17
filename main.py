e7fd752febbcb485d1fe1574941f07082086d054
Hiten Sharma <sharmahiten3@gmail.com>
2017-06-17 21:57:25 +0530

    Liitle bit of formatting

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# !usr/bin/python
+#!usr/bin/python
 
 from __future__ import print_function
 
@@ -16,37 +16,6 @@ if six.PY2:
     import urlparse
 else:
     import urllib.parse as urlparse
-
-
-parser = argparse.ArgumentParser(
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-    description='Download icons from https://shareicon.net')
-parser.add_argument(
-    '-u', '--url', type=str, help='Iconset URL', required=True)
-parser.add_argument(
-    '-p', '--path', type=str, help='Path to save the icons', required=False,
-    default='.')
-parser.add_argument('-f', '--format', nargs='+',
-                    help='Format of the icon to download (Available choices: png, svg, icns, ico)',
-                    type=str,
-                    default=["ico"],
-                    required=False)
-parser.add_argument('-s', '--dimensions', nargs='+',
-                    help='Dimension of the icon to download (Available choices: 12, 16, 32, 48, 64, 128, 256, 512)',
-                    type=int,
-                    default=[512],
-                    required=False)
-
-args = parser.parse_args()
-url = args.url
-down_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), args.path)
-dims = args.dimensions
-formats = args.format
-
-parsed_uri = urlparse.urlparse(url)
-base_url = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
-
-count = 0
 
 
 def get_download_url(url, size=512, flag=False):
@@ -93,21 +62,6 @@ def download_url(url, title=None, ext=None, dim=None, path=None):
     except Exception as e:
         print('[ERROR]: {} -- {}'.format(e, url))
 
-page = requests.get(url)
-tree = html.fromstring(page.content)
-container = tree.xpath('//div[@class="page-container"]')[0]
-
-title = container.xpath('.//h1[contains(@class, "page-title")]/text()')[0]
-
-make_dir(title)
-for ext in formats:
-    path = os.path.join(down_path, title)
-    dir_name = os.path.join(path, ext)
-    make_dir(dir_name)
-    for dim in dims:
-        dim_dir_name = os.path.join(dir_name, '{}x{}'.format(dim, dim))
-        make_dir(dim_dir_name)
-
 
 def download(url, page=None):
     global count
@@ -135,9 +89,60 @@ def download(url, page=None):
                         url = get_download_url(extension)
                     download_url(url, title, ext, dim)
 
-pages = container.xpath(
-    './/ul[@class="pagination"]/li[not(@class="active")]/a/@href')
+def main():
 
-download(url, page)
-for page in pages:
-    download(get_download_url(page))
+    page = requests.get(url)
+    tree = html.fromstring(page.content)
+    container = tree.xpath('//div[@class="page-container"]')[0]
+
+    title = container.xpath('.//h1[contains(@class, "page-title")]/text()')[0]
+
+    make_dir(title)
+    for ext in formats:
+        path = os.path.join(down_path, title)
+        dir_name = os.path.join(path, ext)
+        make_dir(dir_name)
+        for dim in dims:
+            dim_dir_name = os.path.join(dir_name, '{}x{}'.format(dim, dim))
+            make_dir(dim_dir_name)
+
+
+    pages = container.xpath(
+        './/ul[@class="pagination"]/li[not(@class="active")]/a/@href')
+
+    download(url, page)
+    for page in pages:
+        download(get_download_url(page))
+
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    description='Download icons from https://shareicon.net')
+parser.add_argument(
+    '-u', '--url', type=str, help='Iconset URL', required=True)
+parser.add_argument(
+    '-p', '--path', type=str, help='Path to save the icons', required=False,
+    default='.')
+parser.add_argument('-f', '--format', nargs='+',
+                    help='Format of the icon to download (Available choices: png, svg, icns, ico)',
+                    type=str,
+                    default=["ico"],
+                    required=False)
+parser.add_argument('-s', '--dimensions', nargs='+',
+                    help='Dimension of the icon to download (Available choices: 12, 16, 32, 48, 64, 128, 256, 512)',
+                    type=int,
+                    default=[512],
+                    required=False)
+
+args = parser.parse_args()
+url = args.url
+down_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), args.path)
+dims = args.dimensions
+formats = args.format
+
+parsed_uri = urlparse.urlparse(url)
+base_url = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+
+count = 0
+
+if __name__ == '__main__':
+    main()
